f0ed1896a4821a3fa8237c46a92bb4ad0bad0fb5
Kuchta Rafał <RKuchta@multikino.grupa.iti>
2020-01-16 14:05:36 +0100

    Updates

---
+++ precache-manifest.18478f3efdc9cc6859d9e315407ef9bd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "810697e4feaabc8daa66de853bcb5722",
+    "url": "/React-snake/index.html"
+  },
+  {
+    "revision": "ed87f4f756698d8ba746",
+    "url": "/React-snake/static/css/main.04b67aed.chunk.css"
+  },
+  {
+    "revision": "1ca61dd943c29ee9204b",
+    "url": "/React-snake/static/js/2.57e9b755.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-snake/static/js/2.57e9b755.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed87f4f756698d8ba746",
+    "url": "/React-snake/static/js/main.96924e37.chunk.js"
+  },
+  {
+    "revision": "ce574ae79a272e8c8e1b",
+    "url": "/React-snake/static/js/runtime-main.5b9a445b.js"
+  }
+]);
